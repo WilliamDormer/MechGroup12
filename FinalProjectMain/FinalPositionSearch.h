e7988c8896c5c22c382e61aca7a6cfda90c8882f
JakeVanker <85713226+JakeVanker@users.noreply.github.com>
2021-06-13 13:54:20 -0400

    added code to search algorithm

--- FinalProjectMain/FinalPositionSearch.h
+++ FinalProjectMain/FinalPositionSearch.h
@@ -17,10 +17,46 @@ void turnAndGo(){
 }
 
 
+<<<<<<< Updated upstream
   
   void ForwardAndBackward(){
     
   unsigned long timer = millis();
+=======
+    if((i-1)!= -1){
+       positionArray[i][0] = positionArray[i-1][0] + sin(angle)*currentRadius;//x location
+       positionArray[i][1] = positionArray[i-1][1] + cos(angle)*currentRadius; //y location
+    }
+    else{
+       positionArray[i][0] = sin(angle)*currentRadius;//x location
+       positionArray[i][1] = cos(angle)*currentRadius; //y location
+    }
+    
+    //Serial.print("X: ");
+    //Serial.println(positionArray[i][0]);
+    //Serial.print(", Y: ");
+    Serial.println(positionArray[i][1]);
+ }
+ Serial.println("Done");
+    
+    bool flag = false;
+    for(int i = 0; i < arraySize; i++){
+      float nextPoint = distanceToTarget(positionArray[i][0], positionArray[i][1]);
+      while(nextPoint > 3.0 || flag == true){
+        ReadBottomIR();
+        nextPoint = distanceToTarget(positionArray[i][0], positionArray[i][1]);
+        TravelToDestination(positionArray[i][0], positionArray[i][1]);
+          if(setPointBottomPerm - BottomAverage > IRTHRESHOLD || setPointBottomPerm - BottomAverage < -IRTHRESHOLD){
+            flag = true;
+          }
+      }
+      if(i < arraySize){
+        Serial.println("ERROR: Target not in vicinity.");
+        break;
+      }
+    }  
+  //then generate an array of that size. 
+>>>>>>> Stashed changes
   
   left_motor.setSpeed(180); // adjust speed values applied to each motor using the updates from the PID controller
   right_motor.setSpeed(180); 
